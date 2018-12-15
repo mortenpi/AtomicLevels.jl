622363d5b306da554ab4126dd857a01541e6efcc
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-12-15 09:50:58 +0100

    Missing test included, fixed test

--- test/excited_configurations.jl
+++ test/excited_configurations.jl
@@ -1,4 +1,4 @@
 @testset "Excited configurations" begin
-    @test excited_configurations(c"1s2", os"2[s-d]") ==
+    @test excited_configurations(c"1s2", os"2[s-p]"...) ==
         [c"1s2", c"1s 2s", c"2s2", c"2p-2", c"2p- 2p", c"2p2"]
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 
 include("orbitals.jl")
 include("configurations.jl")
+include("excited_configurations.jl")
 include("terms.jl")
 include("jj_terms.jl")
 include("csfs.jl")
