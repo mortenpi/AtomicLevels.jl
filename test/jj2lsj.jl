a90c34ce7f3d5304cb34064248181c0aa183f2f8
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-01-23 22:05:26 +0100

    Better test for jj2lsj

--- test/jj2lsj.jl
+++ test/jj2lsj.jl
@@ -1,5 +1,10 @@
+using LinearAlgebra
+
 @testset "jj2lsj" begin
     # These are not real tests, they only make sure that the
     # generation does not crash.
-    jj2lsj(ros"1[s] 2[s-p] k[s-d]"...)
+    R = jj2lsj(ros"1[s] 2[s-p] k[s-d]"...)
+    # Since it is a rotation matrix, its inverse is simply the
+    # transpose.
+    @test norm(inv(Matrix(R)) - R') < 10eps()
 end
